1d28aa719027c01bfa5c251fe7465bde56e96fc6
pedrochiossi <pchiossi@gmail.com>
2020-08-18 20:39:45 -0300

    complete TransactionsRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, transaction) => {
+        return acc + transaction.value;
+      }, 0);
+    const totalOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, transaction) => acc + transaction.value, 0);
+    return {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+    this.transactions.push(newTransaction);
+    return newTransaction;
   }
 }
 
